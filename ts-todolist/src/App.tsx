462e0dd1d26501f4a2d9ed285c40ec0119411e26
Anastasia Hur'yanova <anastasia39@hotmail.it>
2023-02-22 16:46:53 +0100

    Initializes the app

---
+++ ts-todolist/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import InputField from "./components/InputField";
+
+
+const App :React.FC = () => {
+  return (<div className="App">
+    <span className="heading">Taskify</span>
+    <InputField></InputField>
+    </div>)
+}
+
+export default App;
