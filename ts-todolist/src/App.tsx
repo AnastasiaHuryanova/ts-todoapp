b3301628c32fff7c6c9cc148c250179b1e48882d
Anastasia Hur'yanova <anastasia39@hotmail.it>
2023-02-23 09:53:51 +0100

    Sets states and implements input field

--- ts-todolist/src/App.tsx
+++ ts-todolist/src/App.tsx
@@ -1,12 +1,32 @@
+import React, { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
+import { Todo } from "./model";
 
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-const App :React.FC = () => {
-  return (<div className="App">
-    <span className="heading">Taskify</span>
-    <InputField></InputField>
-    </div>)
-}
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+  console.log(todos);
+  return (
+    <div className="App">
+      <span className="heading">Taskify</span>
+      <InputField
+        todo={todo}
+        setTodo={setTodo}
+        handleAdd={handleAdd}
+      ></InputField>
+   
+    </div>
+  );
+};
 
 export default App;
